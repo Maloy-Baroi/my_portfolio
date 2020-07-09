838c536671b1935bad2039c747b53bf809a2e68e
Team-Explorer171 <61952834+Team-Explorer171@users.noreply.github.com>
2020-07-09 20:51:24 +0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+function service_open(){
+    document.getElementById("service").style.display = 'block';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
+}
+
+function projects_open(){
+    document.getElementById("service").style.display = 'none';
+    document.getElementById('projects').style.display = 'block';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
+}
+
+function contact_open(){
+    document.getElementById("service").style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('contact').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
+}
+
+function about_open(){
+    document.getElementById("service").style.display = 'none';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+    document.getElementById('about').style.display = 'block';
+}
+
+function text_to_clipboard() {
+    var copyText = document.getElementById("email");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Copied the text: " + copyText.value);
+  }
